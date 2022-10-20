fbed4a75f9efa3a206fa739931876d951ad2aadc
URK21CS1203-Arun <109951968+URK21CS1203-Arun@users.noreply.github.com>
2022-10-20 20:50:32 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -31,27 +31,30 @@ def inorder(root) -> None:
     # Write your code here
     if root is None:
         return
-    inorder(root.left_child)
-    print(root.data, end = "")
-    inorder(root.right_child)
+    else:
+        inorder(root.left_child)
+        print(root.data, end = "")
+        inorder(root.right_child)
 
 
 def preorder(root) -> None:
     # Write your code here
     if root is None:
         return
-    print(root.data, end = "")
-    preorder(root.left_child)
-    preorder(root.right_child)
+    else:
+        print(root.data, end = "")
+        preorder(root.left_child)
+        preorder(root.right_child)
 
 
 def postorder(root) -> None:
     # Write your code here
     if root is None:
         return
-    postorder(root.left_child)
-    postorder(root.right_child)
-    print(root.data, end = "")
+    else:
+        postorder(root.left_child)
+        postorder(root.right_child)
+        print(root.data, end = "")
 
 
 # Do not change the following code
